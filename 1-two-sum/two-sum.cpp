4319eebbe74798abba9e94c42d855f9c610bab31
Shivam Kumar <jaiswalshivam95085@gmail.com>
2026-08-06 17:25:41 +0530

    Time: 38 ms (28.45%) | Memory: 13.9 MB (96.56%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -15,28 +15,19 @@ public:
 
 //         return new int[]{};
 int n=nums.size();
-// for(int i=0;i<n;i++)
-// {
-//     for(int j=i+1;i<n;i++)
-//     {
-//         if(nums[i]+nums[j]==target)
-//         {
-//             return {i,j};
-//         }
-        
-//     }
-// }
-//         return {};
-for (int i = 0; i < nums.size(); i++) {
-
-            for (int j = i + 1; j < nums.size(); j++) {
-
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
-            }
+for(int i=0;i<n;i++)
+{
+    for(int j=i+1;j<n;j++)
+    {
+        if(nums[i] + nums[j] == target)
+        {
+            return {i,j};
         }
-
+        
+    }
+}
         return {};
+
+
     }
 };
