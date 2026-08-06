491609a1a71ce9792d9a67c1ec8a778fa6f62788
Shivam Kumar <jaiswalshivam95085@gmail.com>
2026-08-06 17:22:33 +0530

    Time: 45 ms (14.59%) | Memory: 14 MB (96.56%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+// HashMap <Integer, Integer> map = new HashMap<>();
+
+//         for (int i = 0; i < nums.length; i++) {
+//             int jarurat = target - nums[i];
+
+//             if (map.containsKey(jarurat)) {
+//                 return new int[]{i, map.get(jarurat)};
+//             }
+
+//             map.put(nums[i], i);
+//         }
+
+//         return new int[]{};
+int n=nums.size();
+// for(int i=0;i<n;i++)
+// {
+//     for(int j=i+1;i<n;i++)
+//     {
+//         if(nums[i]+nums[j]==target)
+//         {
+//             return {i,j};
+//         }
+        
+//     }
+// }
+//         return {};
+for (int i = 0; i < nums.size(); i++) {
+
+            for (int j = i + 1; j < nums.size(); j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+
+        return {};
+    }
+};
