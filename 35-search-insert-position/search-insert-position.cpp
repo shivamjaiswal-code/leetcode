c64fe9beea46d72bb02c5e1e28d913d2e89b592c
Shivam Kumar <jaiswalshivam95085@gmail.com>
2026-08-09 11:51:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (44.08%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,3 +1,18 @@
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             if(nums[i] >= target)
+//             {
+//                 return i;
+//             }
+//         }
+
+//         return nums.size();
+//     }
+// };
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
@@ -13,3 +28,4 @@ public:
         return nums.size();
     }
 };
+
